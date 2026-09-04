12013cc0cf5529804ce84ad546aefc157d041aa5
Nena <N.zuehlke.inf24@stud.bbbaden.ch>
2026-09-04 11:18:47 +0200

    Add files via upload

---
+++ Models/Verben.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lernperiode_11.Models
+{
+    public class Verb
+    {
+        public string Infinitiv { get; set; } = string.Empty;
+
+        public string[] Formen { get; set; } = [];
+    }
+}
